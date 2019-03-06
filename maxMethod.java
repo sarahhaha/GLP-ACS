e72ad806c2259a3afcc85ac8073e7e6586c18af7
Sora <40410828+chang-s@users.noreply.github.com>
2019-03-06 09:14:37 +0900

    Template for the max method

---
+++ maxMethod.java
@@ -0,0 +1,10 @@
+// Returns the largest value in the given array.
+public static int max(int data[10]) {
+    int max = 0;
+    for (int i = 0; i < data[].length(); i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max[];
+}
